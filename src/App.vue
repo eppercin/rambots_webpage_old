f33309464b1661c785511faa2070086bc794f575
eppercin <percinet@colostate.edu>
2022-09-02 23:32:26 -0600

    vue setup

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app>
+
+        <v-app-bar app color="#1E4D2B" >
+            <v-app-bar-nav-icon color="white"> </v-app-bar-nav-icon>
+            <v-img max-width="201px" :src="require('./assets/CSUWatermarkSmall.png')"></v-img>
+            <v-spacer></v-spacer>
+
+            <template v-slot:extension >
+                <v-flex class="ml-n10">
+                    <v-tabs color="#1E4D2B" align-with-title>
+                        <v-tab>Home</v-tab>
+                        <v-tab>Timeline</v-tab>
+                        <v-tab>Team</v-tab>
+                        <v-tab>Resources</v-tab>
+                    </v-tabs>
+                </v-flex>
+               
+            </template>
+        </v-app-bar>
+
+     
+        <v-main>
+
+            <router-view />
+        </v-main>
+
+
+
+    </v-app>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            model: 0,
+            colors: [
+                'primary',
+                'secondary',
+                'yellow darken-2',
+                'red',
+                'orange',
+            ],
+        }),
+    }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+
+
+    <v-container>
+        <v-row class="mt-4">
+            <v-col cols="4">
+                <v-img contains max-width="600" :src="require('../assets/RamLogoText.png')"></v-img>
+            </v-col>
+            <v-col cols="8">
+                <v-carousel>
+                    <v-carousel-item v-for="(item,i) in items"
+                                     :key="i"
+                                     :src="item.src"
+                                     reverse-transition="fade-transition"
+                                     transition="fade-transition"></v-carousel-item>
+                </v-carousel>
+            </v-col>
+
+
+        </v-row>
+
+
+      
+
+        <v-row class="text-center">
+            <v-col cols="12">
+
+
+                
+
+            </v-col>
+
+            <v-col>
+                <v-img :src="logo"></v-img>
+            </v-col>
+
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                logo: require('../assets/chassis.jpg'),
+                items: [
+                    {
+                        src: require('../assets/chassis.jpg'),
+                    },
+                    {
+                        src: require('../assets/chassis2.jpg'),
+                    },
+                    {
+                        src: require('../assets/RamLogoLarge.png'),
+                    },
+                ],
+            }
+        },
+    }
+</script>
